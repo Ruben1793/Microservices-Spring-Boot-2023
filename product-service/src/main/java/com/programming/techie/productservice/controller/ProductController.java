e2fe619aacda1d99ef4d4a8d621c5d1b9f7ce3f9
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-07-20 17:00:14 -0600

    Creating Parent project and Product Service Module

---
+++ product-service/src/main/java/com/programming/techie/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.programming.techie.productservice.controller;
+
+import com.programming.techie.productservice.dto.ProductRequest;
+import com.programming.techie.productservice.dto.ProductResponse;
+import com.programming.techie.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
---
+++ product-service/src/main/java/com/programming/techie/productservice/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.programming.techie.productservice.service;
+
+import com.programming.techie.productservice.dto.ProductRequest;
+import com.programming.techie.productservice.dto.ProductResponse;
+import com.programming.techie.productservice.model.Product;
+import com.programming.techie.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor // constructor annotation for bean injection
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository; // uses constructor annotation for constructor injection
+
+    public void createProduct (ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        Product newProduct = productRepository.save(product);
+        log.info("Product {} is saved" , product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products  = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}

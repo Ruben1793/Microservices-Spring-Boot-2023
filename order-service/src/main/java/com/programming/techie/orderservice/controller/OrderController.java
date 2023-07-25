a16067282ef2275fb31909ced1e06221dd639690
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-07-25 16:33:08 -0600

    Order Service

---
+++ order-service/src/main/java/com/programming/techie/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.programming.techie.orderservice.controller;
+
+import com.programming.techie.orderservice.dto.OrderRequest;
+import com.programming.techie.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed successfully";
+    }
+}

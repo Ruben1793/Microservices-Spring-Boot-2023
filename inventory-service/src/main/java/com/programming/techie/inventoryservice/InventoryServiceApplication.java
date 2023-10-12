5ea35def4e293a0eb3dc68d6e0060d35aaf9f8c1
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-10-11 18:11:52 -0600

    Discovery Service

--- inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,11 @@
 package com.programming.techie.inventoryservice;
 
-import com.programming.techie.inventoryservice.model.Inventory;
-import com.programming.techie.inventoryservice.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/programming/techie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programming/techie/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final  WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -33,8 +33,8 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)

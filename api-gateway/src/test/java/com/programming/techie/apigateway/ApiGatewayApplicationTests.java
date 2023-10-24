6e05a70165891dd0659ca5ecc454b8fc73af0e3f
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-10-24 15:54:03 -0600

    API Gateway

---
+++ api-gateway/src/test/java/com/programming/techie/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.techie.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

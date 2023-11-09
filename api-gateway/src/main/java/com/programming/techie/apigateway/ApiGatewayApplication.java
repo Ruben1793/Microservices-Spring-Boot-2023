112312e70c4bd18a368f17f8bd37f66d5fc881bb
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-11-08 18:29:18 -0700

    Security

--- api-gateway/src/main/java/com/programming/techie/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/programming/techie/apigateway/ApiGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {

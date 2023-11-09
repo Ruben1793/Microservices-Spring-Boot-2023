f23cfea578db3fe5e0ec356ccecb5ce8f564a238
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2023-11-08 18:31:34 -0700

    removing test unused tests

--- api-gateway/src/test/java/com/programming/techie/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/programming/techie/apigateway/ApiGatewayApplicationTests.java
@@ -1,12 +1,7 @@
 package com.programming.techie.apigateway;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
